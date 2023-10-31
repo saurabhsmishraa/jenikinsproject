dba9dcee07c6abf39495694e70fbdab2a30a6c0c
saurabh mishra <111421281+saurabhsmishraa@users.noreply.github.com>
2023-10-31 12:00:34 +0530

    this is uploading first java file

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello{  
+    public static void main(String[] args){  
+    System.out.println("This is java app \n by using Docker"); 
+    System.out.println("This is java file"); 
+    System.out.println("This is java file91"); 
+    System.out.println("This is java file for building schedule work"); 
+    }  
+    }  
