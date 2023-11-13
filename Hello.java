49c52cf6dcbf27022b600a83a1eb61caf99e3dc6
saurabh mishra <111421281+saurabhsmishraa@users.noreply.github.com>
2023-11-13 21:12:31 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -3,7 +3,5 @@ class Hello{
     System.out.println("This is java app \n by using Docker"); 
     System.out.println("This is java file"); 
     System.out.println("This is java file91"); 
-    System.out.println("This is java file for building schedule work"); 
-        System.out.println("This is java file for building extra schedule work"); 
     }  
     }  
